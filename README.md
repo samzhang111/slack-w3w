69b7c3b1336a82e7c91b1c76c3858d27e46e1d90
Sam <unclesaamm@gmail.com>
2014-07-03 15:08:24 -0500

    slack w3w integration

---
+++ README.md
@@ -0,0 +1,8 @@
+# DSSG slack integrations
+
+The Slack API provides some fun ways to interact with the chat server:
+https://dssg.slack.com/services/2436366266
+
+To adopt for your organization, make a file app/settings.py that contains:
+
+slack_key = <organization's slack key>
---
+++ app/views.py
@@ -0,0 +1,43 @@
+from app import app
+import flask
+from flask import render_template
+from flask import request
+from flask import Response
+from flask import abort
+
+from simplejson import JSONDecodeError
+
+import json
+from math import radians, cos, sin, asin, sqrt
+import requests
+import urllib
+
+from settings import slack_key
+import re
+
+slack_webhook = 'https://dssg.slack.com/services/hooks/incoming-webhook?token={slack_key}'
+w3w_base = 'http://w3w.co/'
+w3w_api = 'http://api.what3words.com/w3w'
+
+is_three_word = re.compile('^\w+?\.\w+?\.\w+$')
+
+@app.route('/receive', methods=['POST'])
+def slack_receive():
+#========================================================================
+# return w3w link
+##===========================================================================
+    
+    msg = request.form['text']
+    chunks = [x.strip() for x in msg.split()]
+
+    responses = []
+    for token in chunks:
+
+        if is_three_word.match(token):
+            # send link
+            response_text = '<{}>'.format(w3w_base + token)
+            responses.append(response_text)
+
+    return Response(json.dumps({'text': '\n'.join(responses)}))
+
+
